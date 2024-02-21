66ba5edf2b1f1b8accbf0c1fc793efbfcaab1d0c
Denis Pronin <de.pronin@gmail.com>
2024-02-21 15:43:54 +0600

    pads and pads movement

--- core/src/com/github/dennispronin/libdgxpong/LibgdxPong.java
+++ core/src/com/github/dennispronin/libdgxpong/LibgdxPong.java
@@ -1,31 +1,104 @@
 package com.github.dennispronin.libdgxpong;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class LibgdxPong extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
 
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private final float windowHeight = 480;
+    private final float windowWidth = 800;
+
+    private final float rectangleWidth = 24;
+    private final float rectangleHeight = 90;
+
+    private static final double RECTANGLE_SPEED = 8d;
+
+    private OrthographicCamera camera;
+    private SpriteBatch spriteBatch;
+    private ShapeRenderer shapeRenderer;
+
+    private Texture ballImage;
+    private Texture rectangleImage;
+    private Sound ballSound;
+
+    private Circle circle;
+    private Rectangle leftRectangle;
+    private Rectangle rightRectangle;
+    private Rectangle centerLine;
+
+    @Override
+    public void create() {
+        ballImage = new Texture(Gdx.files.internal("ball.png"));
+        rectangleImage = new Texture(Gdx.files.internal("rectangle.png"));
+        ballSound = Gdx.audio.newSound(Gdx.files.internal("ball.wav"));
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, windowWidth, windowHeight);
+        spriteBatch = new SpriteBatch();
+        shapeRenderer = new ShapeRenderer();
+
+        leftRectangle = new Rectangle();
+        leftRectangle.y = windowHeight / 2 - rectangleHeight / 2;
+        leftRectangle.width = rectangleWidth;
+        leftRectangle.height = rectangleHeight;
+
+        rightRectangle = new Rectangle();
+        rightRectangle.y = windowHeight / 2 - rectangleHeight / 2;
+        rightRectangle.x = windowWidth - rectangleWidth;
+        rightRectangle.width = rectangleWidth;
+        rightRectangle.height = rectangleHeight;
+
+        centerLine = new Rectangle();
+        centerLine.y = 0;
+        centerLine.x = windowWidth / 2 - 3;
+        centerLine.width = 3;
+        centerLine.height = windowHeight;
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(Color.BLACK);
+        camera.update();
+
+        drawObjects();
+        handleKeyPressed();
+    }
+
+    private void drawObjects() {
+        spriteBatch.setProjectionMatrix(camera.combined);
+        spriteBatch.begin();
+        spriteBatch.draw(rectangleImage, leftRectangle.x, leftRectangle.y, rectangleWidth, rectangleHeight);
+        spriteBatch.draw(rectangleImage, rightRectangle.x, rightRectangle.y, rectangleWidth, rectangleHeight);
+        spriteBatch.draw(rectangleImage, centerLine.x, centerLine.y, 3, windowHeight);
+        spriteBatch.end();
+    }
+
+    private void handleKeyPressed() {
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) leftRectangle.y -= RECTANGLE_SPEED;
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) leftRectangle.y += RECTANGLE_SPEED;
+        if (leftRectangle.y < 0) leftRectangle.y = 0;
+        if (leftRectangle.y > windowHeight - rectangleHeight) leftRectangle.y = windowHeight - rectangleHeight;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) rightRectangle.y -= RECTANGLE_SPEED;
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) rightRectangle.y += RECTANGLE_SPEED;
+        if (rightRectangle.y < 0) rightRectangle.y = 0;
+        if (rightRectangle.y > windowHeight - rectangleHeight) rightRectangle.y = windowHeight - rectangleHeight;
+    }
+
+    @Override
+    public void dispose() {
+        ballImage.dispose();
+        rectangleImage.dispose();
+        ballSound.dispose();
+        spriteBatch.dispose();
+    }
 }
--- desktop/src/com/github/dennispronin/libdgxpong/DesktopLauncher.java
+++ desktop/src/com/github/dennispronin/libdgxpong/DesktopLauncher.java
@@ -2,14 +2,17 @@ package com.github.dennispronin.libdgxpong;
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
-import com.github.dennispronin.libdgxpong.LibgdxPong;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setWindowIcon("icon.png");
+		config.setTitle("Pong");
+		config.setResizable(false);
+		config.setWindowedMode(700, 400);
+		config.useVsync(true);
 		config.setForegroundFPS(60);
-		config.setTitle("Libgdx Pong");
 		new Lwjgl3Application(new LibgdxPong(), config);
 	}
 }
